64f55116d6a2b679887b82202869f69299d95577
Tiagodev <tiagodev@protonmail.com>
2021-09-01 15:50:02 -0700

    add OptForm component and styling

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,6 @@
 import faqsData from "../fixtures/faqs.json";
 import { Accordion } from "../components";
+import OptForm from "../components/OptForm";
 
 export const FaqsContainer = () => (
   <Accordion>
@@ -10,5 +11,14 @@ export const FaqsContainer = () => (
         <Accordion.Body>{item.body}</Accordion.Body>
       </Accordion.Item>
     ))}
+
+    <OptForm>
+      <OptForm.Input placeholder="Email address" />
+      <OptForm.Button>Try it now</OptForm.Button>
+      <OptForm.Break />
+      <OptForm.Text>
+        Ready to watch? Enter your email to create or restart your membership
+      </OptForm.Text>
+    </OptForm>
   </Accordion>
 );
