3e90bf47ca69968701400d2b2993815d6591ff31
Tiagodev <tiagodev@protonmail.com>
2021-09-03 18:02:11 -0700

    add header component and styling

---
+++ src/containers/header.js
@@ -0,0 +1,13 @@
+import { Header } from "../components";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
+
+export const HeaderContainer = ({ children }) => (
+  <Header>
+    <Header.Frame>
+      <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
+      <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
+    </Header.Frame>
+    {children}
+  </Header>
+);
